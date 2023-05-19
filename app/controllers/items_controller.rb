c6a104fdd7a6988c0c5a79d61a123192e7b19042
chicorycoffee <yk.nonogaki@gmail.com>
2023-05-19 21:50:19 +0900

    商品削除機能・修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: :edit
 
@@ -36,11 +36,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
+    if current_user.id == item.user.id
     item.destroy
     redirect_to root_path
+    end
   end
-
-
 
   private
 
